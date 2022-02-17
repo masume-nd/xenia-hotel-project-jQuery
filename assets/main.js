4e69e5627bb9c64d8ed3ec465b42f220a74a4fc3
masoume <masume.ndri@gmail.com>
2022-02-17 21:50:35 +0330

    prosseccing...

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+   $('body').scroll(function(){
+      alert('hi')
+      $('.return-to-top').css({
+         display:"block"
+      })
+   })
    $(function () {
     let slideCount = $(".slider ul li").length;
     let slideWidth = $(".slider ul li").width();
@@ -29,7 +35,7 @@ $(document).ready(function () {
                }
             );
       }
-      const intervalFun =  setInterval(moveRight,6000)
+      //const intervalFun =  setInterval(moveRight,6000)
 
       function moveRight() {
          
@@ -46,69 +52,51 @@ $(document).ready(function () {
                }
             );
       }
-    
-      // $('.firstImg').mouseenter(
-      //    function(){
-      //       $(this).addClass('active').siblings('.firstImg').removeClass('active');
-      // })
-      // $('.firstImg').mouseleave(
-      //    function(){
-      //       $(this).removeClass('active')
-      // })
-      // $('.secondImg').hover(
-      //    function(){
-      //       $(this).addClass('active')
-      // })
-      // $('.secondImg').mouseleave(
-      //    function(){
-      //       $(this).removeClass('active')
-      // })
-      // $('.thirdImg').hover(
-      //    function(){
-      //       $(this).addClass('active')
-      // })
-      // $('.thirdImg').mouseleave(
-      //    function(){
-      //       $(this).removeClass('active')
-      // })
 
-
-      $('.slider-image').mouseenter(()=>{
-            clearInterval(intervalFun)
-         }
-      )
+      // $('.slider-image').mouseenter(()=>{
+      //       clearInterval(intervalFun)
+      //    }
+      // )
    
 
 
 //****************************************************************************
-      
+      $('.UL-slider li').each(function(){
+         $(this).addClass('active').siblings().removeClass('active');
+         if($(".UL-slider li").hasClass('active')){
+            $('.slider-txt').animate({
+               marginTop : "200px",
+            },'slow')    
+         }
+      });
+
       $(".next").on("click", function () {
          moveRight();
-         console.log($(".UL-slider"));
          $('.UL-slider li').each(function(){
             $(this).addClass('active').siblings().removeClass('active');
-            // console.log($(this).hasClass());
             if($(".UL-slider li").hasClass('active')){
                console.log($("hello"));
                $('.slider-txt').animate({
-                  marginTop : "300px",
+                  marginTop : "200px",
                },'slow')    
             }
          });
       })
 
       
-   //    $(".prev").on("click", function () {
-   //       $('.UL-slider li').each(function(){
-   //          $(this).addClass('active').siblings().removeClass('active');
-   //          moveLeft();
-   //    });
-   //    if($('.UL-slider li').hasClass('active')){
-   //       $('.slider-txt').animate({
-   //          marginTop : "300px",
-   //       },'slow')    
-   //    }
-   // })
+      $(".prev").on("click", function () {
+         moveLeft();
+         $('.UL-slider li').each(function(){
+            $(this).addClass('active').siblings().removeClass('active');
+            // console.log($(this).hasClass());
+            if($(".UL-slider li").hasClass('active')){
+               console.log($("hello"));
+               $('.slider-txt').animate({
+                  marginTop : "200px",
+               },'slow')    
+            }
+         });
+   })
 
       // if($('.slider-image').hasClass('.active')){
       //    alert('hello')
@@ -127,10 +115,58 @@ $(document).ready(function () {
 
       $(".homedark").click(function () {
          $("body").css({
-            backgroundColor: "#171720",
+            backgroundColor: "#171720" ,
+         });
+      });
+      $(".homelight").click(function () {
+         $("body").css({
+            backgroundColor: "#f4f4f4" ,
          });
       });
    });
+
+   $(".mainBtn").hover(
+      function () {
+        $(".maintag").css("color", "black");
+        $(".hiddenPart").css({
+          bottom: "0",
+        });
+      },
+      function () {
+        $(".maintag").css("color", "white");
+        $(".hiddenPart").css({
+          bottom: "-40px",
+        });
+      }
+    );
+    $(".mainBtn2").hover(
+      function () {
+        $(".maintag2").css("color", "black");
+        $(".hiddenPart2").css({
+          bottom: "0",
+        });
+      },
+      function () {
+        $(".maintag2").css("color", "white");
+        $(".hiddenPart2").css({
+          bottom: "-40px",
+        });
+      }
+    );
+    $(".mainBtn3").hover(
+      function () {
+        $(".maintag3").css("color", "black");
+        $(".hiddenPart3").css({
+          bottom: "0",
+        });
+      },
+      function () {
+        $(".maintag3").css("color", "white");
+        $(".hiddenPart3").css({
+          bottom: "-40px",
+        });
+      }
+    );
 
    $(".three").click(function () {
       $(".hamburger").toggleClass("is-active");
@@ -150,7 +186,7 @@ $(document).ready(function () {
       $(".drawer-menu").animate(
          {
             width: "25vw",
-            height: "100vh",
+            height: "100%",
          },
          "slow"
       );
@@ -166,9 +202,10 @@ $(document).ready(function () {
          display: "none",
       });
 
-      // $('body').css({
-      //   paddingRight :'25vw'
-      // })
+      $('body').css({
+        position : "fixed"
+
+      })
    }
 
    function closeMenu() {
@@ -176,7 +213,7 @@ $(document).ready(function () {
       $(".drawer-menu").animate(
          {
             width: "0",
-            height: "100vh",
+            height: "100%",
          },
          100
       );
@@ -189,9 +226,9 @@ $(document).ready(function () {
          },
          "slow"
       );
-      // $('body').css({
-      //   paddingRight :'-25vw'
-      // })
+      $('body').css({
+         position : "relative"
+       })
    }
 
 
@@ -212,11 +249,11 @@ $(document).ready(function () {
    const homeMenu = document.getElementById("home");
    $(homeMenu).hover(function () {
       $(".dropdown-content").css("display", "block");
-      $("#ep").css("opacity", "1");
+      $("#ep").css("visibility", "visible");
    });
    $(".dropdown").mouseleave(() => {
       $(".dropdown-content").css("display", "none");
-      $("#ep").css("opacity", ".5");
+      $("#ep").css("visibility", "hidden");
    });
 
    $(".moredropbtn").hover(() => {
@@ -245,6 +282,189 @@ $(document).ready(function () {
          transition: "all ease 2s",
       });
    });
+   $(".reserve").hover(() => {
+      $(".hoverLineReserve").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".reserve").mouseleave(() => {
+      $(".hoverLineReserve").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+  
+    $(".grid").hover(() => {
+      $(".hoverLineGrid").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".grid").mouseleave(() => {
+      $(".hoverLineGrid").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".roomLi").hover(() => {
+      $(".hoverLineLi").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".roomLi").mouseleave(() => {
+      $(".hoverLineLi").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".blog").hover(() => {
+      $(".hoverLineMasonry").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".blog").mouseleave(() => {
+      $(".hoverLineMasonry").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+  
+    $(".test").hover(() => {
+      $(".hoverLineTest").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".test").mouseleave(() => {
+      $(".hoverLineTest").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".shortCods").hover(() => {
+      $(".hoverLineShort").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".shortCods").mouseleave(() => {
+      $(".hoverLineShort").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".sliderRoom").hover(() => {
+      $(".hoverLineSlider").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".sliderRoom").mouseleave(() => {
+      $(".hoverLineSlider").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".sevice").hover(() => {
+      $(".hoverLineRService").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".sevice").mouseleave(() => {
+      $(".hoverLineRService").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".view").hover(() => {
+      $(".hoverLineView").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".view").mouseleave(() => {
+      $(".hoverLineView").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+  
+    $(".singleView").hover(() => {
+      $(".hoverLineSingleView").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".singleView").mouseleave(() => {
+      $(".hoverLineSingleView").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".singleroom").hover(() => {
+      $(".hoverLineSingle").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".singleroom").mouseleave(() => {
+      $(".hoverLineSingle").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".post").hover(() => {
+      $(".hoverLinePost").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".post").mouseleave(() => {
+      $(".hoverLinePost").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".Fullscreen").hover(() => {
+      $(".hoverLineFull").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".Fullscreen").mouseleave(() => {
+      $(".hoverLineFull").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".err").hover(() => {
+      $(".hoverLineErr").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".err").mouseleave(() => {
+      $(".hoverLineErr").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".theme").hover(() => {
+      $(".hoverLineTheme").css({
+        width: "150px",
+        transition: "all ease .5s",
+      });
+    });
+    $(".theme").mouseleave(() => {
+      $(".hoverLineTheme").css({
+        width: "0px",
+        transition: "all ease .5s",
+      });
+    });
    $(".rooms").hover(() => {
       $(".hoverLineRooms").css({
          width: "40px",
@@ -431,9 +651,9 @@ $(document).ready(function () {
          });
       }
    );
-
    $('#arrival-date').datepicker();
    $('#departure-date').datepicker();
+
    $('.button-req').click((e) => {
      let reqArrivalDate = new Date($('#arrival-date').val()).getTime(); 
      let reqDepartureDate= new Date( $('#departure-date').val()).getTime(); 
@@ -457,7 +677,7 @@ $(document).ready(function () {
             let arrivalDate = new Date(Room.from).getTime();
             let departureDate = new Date(Room.to).getTime();
                if(arrivalDate <= reqArrivalDate && departureDate >= reqDepartureDate && numBeds == Room.bed){
-                  room.css({
+                  Room.css({
                      backgroundColor: "yellow"
                   })
                   from.css({
@@ -471,10 +691,6 @@ $(document).ready(function () {
                   })
                } 
           })
-         //  avilableRooms.map(item =>{
-         //    console.log(item);
-         //    item.style.backgroundColor = "red"  
-         //  })
          
           
          },
@@ -482,6 +698,7 @@ $(document).ready(function () {
    })
 });
 let slideIndex = 1;
+let interval_slides = setInterval(showSlides,1000)
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -514,33 +731,33 @@ function showSlides(n) {
 
 //second carousel
 let slideIndexNumber = 1;
-// showLastSlider(slideIndexNumber);
+showLastSlider(slideIndexNumber);
 
-// function plusTheSlides(number) {
-//    showLastSlider((slideIndexNumber += n));
-// }
+function plusTheSlides(number) {
+   showLastSlider((slideIndexNumber += n));
+}
 
-// function currentSlides(number) {
-//    showLastSlider((slideIndexNumber = number));
-// }
+function currentSlides(number) {
+   showLastSlider((slideIndexNumber = number));
+}
 
-// function showLastSlider(number) {
-//   let index;
-//   let slidesAndText = document.getElementsByClassName("textImg trans");
-//   let Dots = document.getElementsByClassName("otherdot");
-//    if (number > slidesAndText.length) {
-//       slidesAndText = 1;
-//    }
-//    if (number < 1) {
-//       slideIndexNumber = slidesAndText.length;
-//    }
-//    for (index = 0; index < slidesAndText.length; index++) {
-//       slidesAndText[index].style.display = "none";
-//    }
-//    for (index = 0; index < Dots.length; index++) {
-//       Dots[index].className = Dots[index].className.replace(" active", "");
-//    }
-//    slidesAndText[slideIndexNumber - 1].style.display = "block";
-//    Dots[slideIndexNumber - 1].className += " active";
-// }
+function showLastSlider(number) {
+  let index;
+  let slidesAndText = document.getElementsByClassName("textImg trans");
+  let Dots = document.getElementsByClassName("otherdot");
+   if (number > slidesAndText.length) {
+      slidesAndText = 1;
+   }
+   if (number < 1) {
+      slideIndexNumber = slidesAndText.length;
+   }
+   for (index = 0; index < slidesAndText.length; index++) {
+      slidesAndText[index].style.display = "none";
+   }
+   for (index = 0; index < Dots.length; index++) {
+      Dots[index].className = Dots[index].className.replace(" active", "");
+   }
+   slidesAndText[slideIndexNumber - 1].style.display = "block";
+   Dots[slideIndexNumber - 1].className += " active";
+}
 
